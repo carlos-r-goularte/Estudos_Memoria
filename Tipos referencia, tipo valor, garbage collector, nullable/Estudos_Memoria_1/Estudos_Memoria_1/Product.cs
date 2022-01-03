d22230a585380a0ded00d71a4423ba6fd750c1d0
Carlos Rodrigo Goularte <carlos.r.goularte@hotmail.com>
2022-01-03 12:03:00 -0300

    Referencia na memoria de classes

---
+++ Tipos referencia, tipo valor, garbage collector, nullable/Estudos_Memoria_1/Estudos_Memoria_1/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos_Memoria_1
+{
+    internal class Product
+    {
+        public double X;
+        public double Y; 
+    }
+}
---
+++ Tipos referencia, tipo valor, garbage collector, nullable/Estudos_Memoria_1/Estudos_Memoria_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Estudos_Memoria_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1,p2;
+            p1 = new Product("TV",1000);
+            p2 = new Product("Radio",500);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+        }
+    }
+}
