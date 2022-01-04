ae7356eb10456ff6df5972150f2cf48e060dd5b8
Carlos Rodrigo Goularte <carlos.r.goularte@hotmail.com>
2022-01-04 11:16:52 -0300

    Estudo Vetores 1

---
+++ Vetores/Vetores/Vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int quantidadeDeLeituras = int.Parse(Console.ReadLine());
+
+            double[] vetor = new double[quantidadeDeLeituras];
+
+            for (int i = 0; i < quantidadeDeLeituras; i++)
+            {
+                vetor[i] = double.Parse(Console.ReadLine());
+
+            }
+
+            double alturaSoma = 0.0;
+
+            for (int i = 0; i < quantidadeDeLeituras; i++)
+            {
+                alturaSoma += vetor[i];
+            }
+
+            double alturaMedia = alturaSoma / quantidadeDeLeituras;
+
+            Console.WriteLine("Altura Média: " + alturaMedia.ToString("F2"));
+        }
+    }
+}
